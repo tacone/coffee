a094300378894522ae9a209426c4159cbf3a4b1e
tacone <tacone@users.noreply.github.com>
2014-12-23 02:11:34 +0100

    implement data source create()

--- lib/DataSource/DataSource.php
+++ lib/DataSource/DataSource.php
@@ -4,24 +4,24 @@
 namespace Tacone\Coffee\DataSource;
 
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Tacone\Coffee\Base\DelegatedArrayTrait;
 
 /**
  * You neighbourly dot syntax data source.
  */
-
-class DataSource  implements \Countable, \IteratorAggregate, \ArrayAccess
+class DataSource implements \Countable, \IteratorAggregate, \ArrayAccess
 {
     use DelegatedArrayTrait;
 
     protected $source;
 
-    public function __construct(&$source)
+    public function __construct($source)
     {
-        $this->source =& $source;
+        $this->source = $source;
     }
 
-    public static function make(&$source)
+    public static function make($source)
     {
         return new static($source);
     }
@@ -29,6 +29,119 @@ class DataSource  implements \Countable, \IteratorAggregate, \ArrayAccess
     protected function getDelegatedStorage()
     {
         return $this->source;
+    }
+
+    public function offsetGet($offset)
+    {
+//        $tokens = explode('.', $offset);
+//        $token = array_shift($tokens);
+//
+//        if (!$tokens) {
+//            return $this->read($token);
+//        }
+//        $source = static::make($this->read($token));
+//        return $source[join('.', $tokens)];
+
+        $key = null;
+        $source = $this->find($offset, $key);
+        if (is_object($source)) {
+            return $source->read($key);
+        }
+        throw new \LogicException("Last source must be object");
+    }
+
+    protected function find($offset, &$key)
+    {
+        $tokens = explode('.', $offset);
+        $key = array_shift($tokens);
+
+        if (!$tokens) {
+            return $this;
+        }
+        $source = static::make($this->read($key));
+        $offset = join('.', $tokens);
+        return $source->find($offset,$key );
+    }
+
+    protected function read($key)
+    {
+        if (isset($this->source->$key)) {
+            return $this->source->$key;
+        }
+        $result = $this->create($key);
+        return $result;
+    }
+
+    protected function create($key)
+    {
+        if (method_exists($this->source, $key))
+        {
+            $result = $this->source->$key();
+            $model = $result->getModel();
+//            var_dump (method_exists($this->source, 'associate')); die;
+            switch(true){
+                case $result instanceof BelongsTo:
+//                    $this->source->associate($model);
+                    $result->associate($model);
+                    break;
+                default:
+                    echo "found method $key of type: ".get_class($result);
+                    echo " leading to model: ".get_class($model);
+                    die;
+            }
+            return $model;
+//            var_dump ($result);
+//            $result = new BelongsTo;
+
+        }
+        throw new \RuntimeException("Creation not supported for key: '$key'");
+    }
+
+    protected function write($key, $value)
+    {
+        $this->source->$key = $value;
+    }
+
+    function offsetSet($offset, $value)
+    {
+
+        $key = null;
+        $source = $this->find($offset, $key);
+        if (is_object($source)) {
+            $source->write($key,$value);
+            return;
+        }
+        throw new \LogicException("Last source must be object");
+
+//        $tokens = explode('.', $offset);
+//        $key = array_shift($tokens);
+//
+//        if (!$tokens) {
+//            $this->write($key, $value);
+//        }
+//
+//        $tokens = join('.', $tokens);
+//
+//
+//
+//        $source = static::make($this->read($token));
+//        return $source[];
+
+
+        $tokens = explode('.', $offset);
+        $target = $this->source;
+        while (count($tokens) > 1) {
+            $key = array_shift($tokens);
+            if (isset($target->$key)) {
+                $target = $target->$key;
+            } else {
+                throw new \RuntimeException("Offset \"$offset\" not found");
+            }
+        }
+        $key = array_shift($tokens);
+        $target->$key = $value;
+
+        return $this;
     }
 
     public function offsetExists($offset)
@@ -45,35 +158,5 @@ class DataSource  implements \Countable, \IteratorAggregate, \ArrayAccess
         return true;
     }
 
-    public function offsetGet($offset)
-    {
-        $offset = explode('.', $offset);
-        $target = $this->source;
-        foreach ($offset as $key) {
-            if (isset($target->$key)) {
-                $target = $target->$key;
-            } else {
-                return null;
-            }
-        }
-        return $target;
-    }
 
-    function offsetSet($offset, $value)
-    {
-        $offset = explode('.', $offset);
-        $target = $this->source;
-        while (count($offset) > 1) {
-            $key = array_shift($offset);
-            if (isset($target->$key)) {
-                $target = $target->$key;
-            } else {
-                die ("offset $offset not found");
-            }
-        }
-        $key = array_shift($offset);
-        $target->$key = $value;
-
-        return $this;
-    }
 }
