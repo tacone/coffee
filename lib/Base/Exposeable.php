593297f5eff17e99bb06579d7aeee93c5d0d0a53
tacone <tacone@users.noreply.github.com>
2014-12-31 23:17:26 +0100

    document more

--- lib/Base/Exposeable.php
+++ lib/Base/Exposeable.php
@@ -6,7 +6,7 @@ namespace Tacone\Coffee\Base;
  * interface from their parent objects.
  *
  * Override the exposes() method to tell the parent which methods to
- * expose as accessors, and which to expose as fluent methods.
+ * expose as accessors, and which to expose as accessors or fluent methods.
  *
  * The parent object will then need to call the handleExposeables()
  * static method to delegate the method call to their exposeable
@@ -17,6 +17,10 @@ namespace Tacone\Coffee\Base;
  */
 trait Exposeable
 {
+    /**
+     * The methods exposed by this property
+     * @return array
+     */
     public function exposes()
     {
         /**
@@ -33,63 +37,101 @@ trait Exposeable
         return [];
     }
 
-    public static function isExposeable($object)
+    /**
+     * Whether a property instance is exposeable or not.
+     *
+     * @param $attribute
+     * @return bool
+     */
+    public static function isExposeable($attribute)
     {
-        if (!is_object($object)) {
+        if (!is_object($attribute)) {
             return false;
         }
-        $traits = class_uses_recursive(get_class($object));
+        $traits = class_uses_recursive(get_class($attribute));
 
         return in_array(__TRAIT__, $traits);
 
     }
 
-    public static function callExposeableMethod($parent, $object, $method = null, $parameters, $return = null)
+    /**
+     * Calls a property method and returns the result or the parent itself.
+     *
+     * @param  object $parent     the parent object
+     * @param  object $property   the chosen parent property
+     * @param  string $method     the name of the method to call
+     * @param  array  $parameters the arguments to pass to the method
+     * @param  null   $return     weather to return the result or the parent object
+     * @return mixed  $result the result returned by the method or the $parent itself
+     */
+    public static function callExposeableMethod($parent, $property, $method = null, $parameters, $return = null)
     {
-        $callback = $method ? [$object, $method] : $object;
+        $callback = $method ? [$property, $method] : $property;
         $result = call_user_func_array($callback, $parameters);
         if (is_bool($return)) {
             return $return ? $result : $parent;
         }
 
         if ($return === null) {
-            return $result === $object ? $parent : $result;
+            return $result === $property ? $parent : $result;
         }
         throw new \LogicException('$return can be either null/true/false, ' . gettype($return) . ' given');
     }
 
+    /**
+     * Cycles $parent and find the applicable exposeable property to call the
+     * requested method on.
+     * The matching is based on a combination of the property name and the
+     * attribute name to be called.
+     *
+     * For example: $parent->removeAttr() may match $parent->attr->remove();
+     * If the attribute implements the __invoke() magic method, then it may
+     * be called directly, ex: $parent->attr();
+     *
+     * @param  object $parent     the object instance that contains the attributes
+     * @param  string $methodName the requested method (i.e.: removeAttr)
+     * @param  array  $parameters the arguments to pass to the method
+     * @return mixed  $result the result returned by the method or the $parent itself
+     */
     public static function handleExposeables(
         $parent,
         $methodName,
         $parameters
     ) {
-        $properties = array_keys(get_object_vars($parent));
-        $properties = array_filter($properties, function ($prop) use ($methodName, $parent) {
+        $parentProperties = array_keys(get_object_vars($parent));
+        $parentProperties = array_filter($parentProperties, function ($prop) use ($methodName, $parent) {
             return stripos($methodName, $prop) !== false
             && is_object($parent->$prop)
             && static::isExposeable($parent->$prop);
         });;
 
-        foreach ($properties as $prop) {
-            if ($methodName === $prop && is_callable($parent->$prop)) {
+        foreach ($parentProperties as $propertyName) {
+            // check if the property implements the __invoke() method
+            if ($methodName === $propertyName && is_callable($parent->$propertyName)) {
                 return static::callExposeableMethod($parent, $parent->$methodName, null, $parameters);
             }
-            $exposeds = $parent->$prop->exposes();
-            $accessors = isset($exposeds['accessors']) ? (array) $exposeds['accessors'] : [];
-            $others = isset($exposeds['others']) ? (array) $exposeds['others'] : [];
 
+            // retrieve the exposed methods
+            $propertyMethods = $parent->$propertyName->exposes();
+            $accessors = isset($propertyMethods['accessors']) ? (array) $propertyMethods['accessors'] : [];
+            $others = isset($propertyMethods['others']) ? (array) $propertyMethods['others'] : [];
+
+            // check if any accessor applies
             foreach ($accessors as $method) {
-                if ($methodName === $method . ucfirst($prop)) {
-                    return static::callExposeableMethod($parent, $parent->$prop, $method, $parameters, false);
+                if ($methodName === $method . ucfirst($propertyName)) {
+                    return static::callExposeableMethod($parent, $parent->$propertyName, $method, $parameters, false);
                 }
             }
+
+            // check the other methods
             foreach ($others as $method) {
-                if ($methodName === $method . ucfirst($prop)) {
-                    return static::callExposeableMethod($parent, $parent->$prop, $method, $parameters, false);
+                if ($methodName === $method . ucfirst($propertyName)) {
+                    return static::callExposeableMethod($parent, $parent->$propertyName, $method, $parameters, false);
                 }
             }
         }
 
+        // the method does not exists or it's been not exposed
         throw new \RuntimeException('Method \'' . get_class($parent) . "::$methodName' does not exists'");
     }
 }
