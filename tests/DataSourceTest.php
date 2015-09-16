c6d12650f48f781db98ca766f764967c7e699494
tacone <tacone@users.noreply.github.com>
2015-09-16 20:50:40 +0200

    more tests

--- tests/DataSourceTest.php
+++ tests/DataSourceTest.php
@@ -2,21 +2,45 @@
 
 namespace Tacone\Coffee\Test;
 
+use Tacone\Coffee\DataSource\DataSource;
+
 class DataSourceTest extends BaseTestCase
 {
     public function testGet()
     {
-        $c = new Customer();
-        $c->name = 'Frank';
-        $c->surname = 'Sinatra';
-        assertEquals('Frank', $c->name);
+        $customer = new Customer();
+        $source = new DataSource($customer);
+        $source['name'] = 'Frank';
+        $source['surname'] = 'Sinatra';
+        assertSame('Frank', $customer->name);
+        assertSame('Frank Sinatra', $customer->full_name);
+        assertSame('Frank', $source['name']);
+        assertSame('Frank Sinatra', $source['full_name']);
+        $source->save();
+        assertSame(1, Customer::all()->count());
+
+        // now let's check if the update works
+        $source['name'] = 'Jake';
+        assertSame('Jake', $source['name']);
+        $source->save();
+        assertSame(1, Customer::all()->count());
+        assertSame('Jake', Customer::find($source['id'])->name);
+
+        // again, and to test cache collisions
+        $customer = new Customer();
+        $source = new DataSource($customer);
+        $source['name'] = 'Brandon';
+        $source['surname'] = 'Lee';
+        assertSame('Brandon', $customer->name);
+        assertSame('Brandon Lee', $customer->full_name);
+        assertSame('Brandon', $source['name']);
+        assertSame('Brandon Lee', $source['full_name']);
+        $source->save();
+
+        assertSame(2, Customer::all()->count());
     }
 
     public function testGet2()
     {
-        $c = new Customer();
-        $c->name = 'Frank';
-        $c->surname = 'Sinatra';
-        assertEquals('Frank', $c->name);
     }
 }
